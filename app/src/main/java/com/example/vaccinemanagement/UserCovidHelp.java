a07d31ae3e91471ce224854a12ab5fb5da4122c6
Shraddha Gulati <interact.shraddha@gmail.com>
2021-05-28 13:20:03 +0530

    Corrected User Help Activity Error

--- app/src/main/java/com/example/vaccinemanagement/UserCovidHelp.java
+++ app/src/main/java/com/example/vaccinemanagement/UserCovidHelp.java
@@ -7,21 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Objects;
+
 public class UserCovidHelp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_covid_help);
-        getSupportActionBar().hide();
-        Button back_b = (Button)findViewById(R.id.back_b);
-        back_b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(UserCovidHelp.this,Home.class);
-                startActivity(myIntent);
-                finish();
-            }
-        });
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+
     }
 }
